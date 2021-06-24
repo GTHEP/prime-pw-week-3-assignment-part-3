5e52d65180b096f3e2b550b0f2dc8c2879a9c21d
Garrett Harty <gharty@live.com>
2021-06-24 16:14:58 -0500

    Solved the stretch goals.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -29,13 +29,14 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
+
 for (let i = 0; i < supplyChanges.length; i++) {
   if (supplyChanges[i] === 0) {
     console.log('No Change.');
   } else if (supplyChanges[i] > 0) {
     console.log(`Added ${supplyChanges[i]} parts.`);
   } else {
-    console.log(`Removed ${supplyChanges[i]} parts`);
+    console.log(`Removed ${Math.abs(supplyChanges[i])} parts`);
   }
 }
 
@@ -43,21 +44,47 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
+
 let x;
+
 for (x of supplyChanges) {
   if (x === 0) {
     console.log('No Change.');
   } else if (x > 0) {
     console.log(`Added ${x} parts.`);
   } else {
-    console.log(`Removed ${x} parts`);
+    console.log(`Removed ${Math.abs(x)} parts`);
   }
 }
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
 
+let i = 0;
+
+while (i < supplyChanges.length) {
+  if (supplyChanges[i] === 0) {
+    console.log('No Change.');
+  } else if (supplyChanges[i] > 0) {
+    console.log(`Added ${supplyChanges[i]} parts.`);
+  } else {
+    console.log(`Removed ${Math.abs(supplyChanges[i])} parts`);
+  }
+  i++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('9. Total supplies available is:');
+
+// Easy way
+let sum = 0;
+
+for (let i = 0; i < supplyChanges.length; i++) {
+  sum += supplyChanges[i];
+}
+console.log(`The total number of parts available is ${sum}.`);
+
+// Hard way - Don't fully understand the reduce function, but hey it works.
+let totalSum = supplyChanges.reduce((a, b) => a + b, 0);
+console.log(`The total number of parts available is ${totalSum}.`);
